0fcc20c660159c5f0d649715e166b3748b1ac9ca
Boris Arturo González Rivera <borisgr04@gmail.com>
2019-11-25 00:40:13 -0500

    Se confirman cambios implementando el Mock con Moq

--- Application.Test/CrearCuentaBancariaServiceTest.cs
+++ Application.Test/CrearCuentaBancariaServiceTest.cs
@@ -1,7 +1,9 @@
 using Application.Test.Fake;
+using Domain.Contracts;
 using Infrastructure;
 using Infrastructure.Base;
 using Microsoft.EntityFrameworkCore;
+using Moq;
 using NUnit.Framework;
 
 namespace Application.Test
@@ -28,6 +30,25 @@ namespace Application.Test
             var request = new CrearCuentaBancariaRequest { Numero = "1111", Nombre = "aaaaa", TipoCuenta = "Ahorro", Email="test@email.com" };
             CrearCuentaBancariaService _service = new CrearCuentaBancariaService(new UnitOfWork(_context), new EmailSenderFake());
             var response = _service.Ejecutar(request);
+            Assert.AreEqual("Se creó con éxito la cuenta 1111.", response.Mensaje);
+        }
+
+        [Test]
+        public void CrearCuentaBancariaMoqTest()
+        {
+            // create mock version
+            var mockDependency = new Mock<IEmailSender>();
+
+            // set up mock version's method
+            mockDependency.Setup(x => x.SendEmailAsync(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>()));
+
+            var request = new CrearCuentaBancariaRequest { Numero = "1111", Nombre = "aaaaa", TipoCuenta = "Ahorro", Email = "test@email.com" };
+            CrearCuentaBancariaService _serviceSut = new CrearCuentaBancariaService(new UnitOfWork(_context), mockDependency.Object);
+            var response = _serviceSut.Ejecutar(request);
+
+            // Assert that the  method was called with the parameters expecteds
+            mockDependency.Verify(x => x.SendEmailAsync(request.Email, "Cuenta Creada!!", $"Se ha creado cuenta bancaria número {request.Numero}"), Times.Once);
+
             Assert.AreEqual("Se creó con éxito la cuenta 1111.", response.Mensaje);
         }
     }
